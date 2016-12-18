2f19fdf5f8848d9c4ef190194ab0010b7cc4722b
yishai <yishai@glide.me>
2016-12-18 22:21:35 +0200

    yay! all is good

--- app/src/main/java/com/cadaloco/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/cadaloco/sunshine/ForecastAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.v4.widget.CursorAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,72 +22,46 @@ import com.cadaloco.sunshine.utils.Utility;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder> {
 
-    protected final Context mContext;
-
-    protected CursorAdapter mCursorAdapter;
-
-    private ForecastViewHolder mViewHolder;
+    Cursor cursor;
 
     public ForecastAdapter(Context mContext) {
         LogUtil.logMethodCalled();
-        this.mContext = mContext;
-
-        initCursorAdapter(mContext);
     }
 
     // init cursor adapter
-    private void initCursorAdapter(final Context mContext) {
-        LogUtil.logMethodCalled();
-
-        mCursorAdapter = new CursorAdapter(mContext, null, false) {
-            @Override
-            public View newView(Context context, Cursor cursor, ViewGroup parent) {
-                LogUtil.logMethodCalled();
-
-                return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-            }
-
-            @Override
-            public void bindView(View view, Context context, final Cursor cursor) {
-                LogUtil.logMethodCalled();
-
-                mViewHolder.getmForecast().setText(convertCursorRowToUXFormat(cursor));
-
-                mViewHolder.getmView().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LogUtil.logMethodCalled();
-                        if (cursor != null) {
-                            Context context = view.getContext();
-                            String locationSetting = Utility.getPreferredLocation(context);
-                            Uri uri = WeatherContract.WeatherEntry
-                                    .buildWeatherLocationWithDate(
-                                            locationSetting,
-                                            cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
-                            Intent intent = new Intent(context, DetailActivity.class).setData(uri);
-                            String txt = Utility.generateSimpleText(cursor, Utility.isMetric(context));
-                            Toast.makeText(context, uri.toString() + " was clicked!\n" +txt, Toast.LENGTH_SHORT).show();
-
-                            context.startActivity(intent);
-                        }
-                    }
-                });
-            }
-        };
-    }
 
     //View Holder
     public class ForecastViewHolder extends RecyclerView.ViewHolder{
 
         private final View mView;
         private TextView mForecast;
+        public long date;
+        public String funnyText;
+        public View.OnClickListener mListener;
 
         public ForecastViewHolder(View itemView) {
             super(itemView);
             LogUtil.logMethodCalled();
             mView = itemView;
 
+            mListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    LogUtil.logMethodCalled();
+                        Context context = view.getContext();
+                        String locationSetting = Utility.getPreferredLocation(context);
+                        Uri uri = WeatherContract.WeatherEntry
+                                          .buildWeatherLocationWithDate(
+                                                  locationSetting,
+                                                  date);
+                        Intent intent = new Intent(context, DetailActivity.class).setData(uri);
+//                        String txt = Utility.generateSimpleText(cursor, Utility.isMetric(context));
+                        Toast.makeText(context, uri.toString() + " was clicked!\n" +funnyText, Toast.LENGTH_SHORT).show();
+
+                        context.startActivity(intent);
+                }
+            };
+            mView.setOnClickListener(mListener);
             mForecast = (TextView) itemView.findViewById(R.id.list_item_forecast_textView);
         }
 
@@ -107,27 +80,32 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
     @Override
     public ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LogUtil.logMethodCalled();
-        return new ForecastViewHolder(mCursorAdapter.newView(mContext, mCursorAdapter.getCursor(), parent));
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_forecast, parent, false);
+        return new ForecastViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ForecastViewHolder holder, int position) {
         LogUtil.logMethodCalled();
+        if (cursor == null) {
+            return;
+        }
         // Move cursor to this position
-        mCursorAdapter.getCursor().moveToPosition(position);
+        cursor.moveToPosition(position);
 
         // Set the ViewHolder
-        this.mViewHolder = holder;
-
+        holder.date = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        holder.funnyText = Utility.generateSimpleText(cursor, Utility.isMetric(holder.getmView().getContext()));
         // Bind this view
-        mCursorAdapter.bindView(null, mContext, mCursorAdapter.getCursor());
+        holder.getmForecast().setText(convertCursorRowToUXFormat(cursor, holder.getmView().getContext()));
+//        mCursorAdapter.bindView(null, mContext, cursor);
 
     }
 
     @Override
     public int getItemCount() {
         LogUtil.logMethodCalled();
-        return mCursorAdapter.getCount();
+        return cursor == null ? 0 :cursor.getCount();
     }
 
 
@@ -140,23 +118,24 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
     public void swapCursor(Cursor cursor) {
         LogUtil.logMethodCalled();
 
-        this.mCursorAdapter.swapCursor(cursor);
+        this.cursor = cursor;
         notifyDataSetChanged();
     }
 
-    private String convertCursorRowToUXFormat(Cursor cursor) {
+    private String convertCursorRowToUXFormat(Cursor cursor, Context context) {
 
         String highAndLow = formatHighLows(
                 cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP),
-                cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP));
+                cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP),
+                context);
 
         return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE)) +
                 " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC) +
                 " - " + highAndLow;
     }
 
-    private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
+    private String formatHighLows(double high, double low, Context context) {
+        boolean isMetric = Utility.isMetric(context);
         String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
         return highLowStr;
     }
